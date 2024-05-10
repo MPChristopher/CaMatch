5c78c7bcdd76a09f7ae73deafb659ad978f63c30
unknown <papiarceus@gmail.com>
2024-05-10 11:06:43 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  entradas: any[] = [];
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async crearNuevaEntrada() {
+    const foto = await this.tomarFoto();
+    if (foto) {
+      const titulo = prompt('Ingrese un título para la entrada:');
+      const descripcion = prompt('Ingrese una descripción para la entrada:');
+
+      const nuevaEntrada = { foto, titulo, descripcion };
+      
+      this.entradas.push(nuevaEntrada);
+    }
+  }
+
+  async tomarFoto() {
+    const foto = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    if (foto && foto.webPath) {
+      return foto.webPath;
+    } else {
+      return null;
+    }
+  }
+
+  async eliminarEntrada(entrada: any) {
+    const alert = await this.alertController.create({
+      header: 'Confirmar eliminación',
+      message: '¿Estás seguro de que quieres eliminar esta publicacion?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Eliminar',
+          handler: () => {
+            const index = this.entradas.indexOf(entrada);
+            if (index !== -1) {
+              this.entradas.splice(index, 1);
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
